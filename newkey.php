fc638ddcefae285bab0d1f965cc5db843f617006
Herson Cruz <herson@multisistemas.com.sv>
2020-07-21 20:59:38 -0600

    Initial commit

---
+++ newkey.php
@@ -0,0 +1,13 @@
+<?php
+require __DIR__ . '/vendor/autoload.php';
+//include('Crypt/RSA.php');
+use \phpseclib\Crypt\RSA;
+
+$rsa = new RSA();
+$rsa->setPublicKeyFormat('CRYPT_RSA_PUBLIC_FORMAT_OPENSSH');
+$result = $rsa->createKey();
+echo "<pre>";
+echo $result["privatekey"];
+echo "\r\n\r\n";
+echo $result["publickey"];
+?>
